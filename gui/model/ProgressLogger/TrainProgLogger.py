c70b583747c075b0fde97643c6fe45f3f7169a5a
mikigatto93 <mikigatto93@gmail.com>
2023-01-15 01:06:31 +0100

    fix e test

--- gui/model/ProgressLogger/TrainProgLogger.py
+++ gui/model/ProgressLogger/TrainProgLogger.py
@@ -8,21 +8,4 @@ class TrainProgLogger(TimeLogger):
         if len(tokens) > 1:
             rem = tokens[3].replace("remaining", "").strip()
             tot = tokens[4].strip()
-
-            # tot_min = parse("{:g}m", tot)
-            # tot_sec = parse("{:g}s", tot)
-            # tot_ms = parse("{:g}ms", tot)
-            # tot_us = parse("{:g}us", tot)
-            #
-            # rem_min = parse("{:g}m", rem)
-            # rem_sec = parse("{:g}s", rem)
-            # rem_ms = parse("{:g}ms", rem)
-            # rem_us = parse("{:g}us", rem)
-            #
-            # obj = {
-            #     'min': _validate_value(rem_min),
-            #     'sec': _validate_value(rem_sec),
-            #     'ms': _validate_value(rem_ms),
-            #     'us': _validate_value(rem_us)
-            # }
             self.add_to_stack('elapsed: {}, remaining: {}'.format(rem, tot))
